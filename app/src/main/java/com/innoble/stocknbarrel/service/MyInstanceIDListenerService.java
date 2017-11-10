7594d2a4731712927c40e63d921f7a1f2c4f6d84
Sheldon Hall <sheldonhhall@gmail.com>
2017-11-10 19:49:21 -0400

    Implementing firebase

---
+++ app/src/main/java/com/innoble/stocknbarrel/service/MyInstanceIDListenerService.java
@@ -0,0 +1,35 @@
+package com.innoble.stocknbarrel.service;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by sheldonhall on 11/10/17.
+ */
+
+
+public class MyInstanceIDListenerService extends FirebaseInstanceIdService {
+
+
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is also called
+     * when the InstanceID token is initially generated, so this is where
+     * you retrieve the token.
+     */
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        // TODO: Implement this method to send any registration to your app's servers.
+        //sendRegistrationToServer(refreshedToken);
+    }
+
+}
