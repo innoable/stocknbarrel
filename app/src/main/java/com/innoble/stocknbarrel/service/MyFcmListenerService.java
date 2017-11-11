a6e28b2101b0e3637c4e66212f57ecac0ec801c2
Sheldon Hall <sheldonhhall@gmail.com>
2017-11-11 08:31:29 -0400

    Implementing firebase cloud messaging

---
+++ app/src/main/java/com/innoble/stocknbarrel/service/MyFcmListenerService.java
@@ -0,0 +1,19 @@
+package com.innoble.stocknbarrel.service;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+
+/**
+ * Created by sheldonhall on 11/11/17.
+ */
+
+public class MyFcmListenerService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage message){
+        String from = message.getFrom();
+        Map data = message.getData();
+    }
+
+}
